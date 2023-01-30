dd2de4229c610316427610e9d23600cc9e68f450
Junjie_Li <jfjunjieli@gmail.com>
2023-01-31 00:20:57 +0100

    header-footer: init

--- arcoirsl_webstore/resources/views/index.blade.php
+++ arcoirsl_webstore/resources/views/index.blade.php
@@ -1,17 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>arcoirsl</title>
+
     <link href="../css/tailwind.css" rel="stylesheet">
+    <link href="../css/app.css" rel="stylesheet">
+
 </head>
+
 <body>
-    
+
+    <button class="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">...</button>
+
+
     <h1 class="text-3xl font-bold underline">
-      hello junjieli
+        hello junjieli
     </h1>
 
 </body>
+
 </html>
