f73603a521fed4d93a60a237308d813ddfddf897
Junjie_Li <jfjunjieli@gmail.com>
2023-01-31 21:12:02 +0100

    feat: header&footer demo

--- arcoirsl_webstore/resources/views/index.blade.php
+++ arcoirsl_webstore/resources/views/index.blade.php
@@ -1,29 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>arcoirsl</title>
-    {{-- 
-    <link href="../css/app.css" rel="stylesheet">
-    <link href="../css/normalize.css" rel="stylesheet">
-    <link href="../css/tailwind.css" rel="stylesheet"> --}}
 
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/normalize.css') }}" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/tailwind.css') }}" />
-
-</head>
+@include('partials._head')
 
 <body>
 
-    <a href="http://goo">wqeqw</a>
+    @include('partials._header')
 
-    <h1 class="text-3xl font-bold underline">
-        hello junjieli
-    </h1>
+    @include('partials._nav')
+
+    <div class="container">
+        @yield('content')
+    </div>
+
+    @include('partials._footer')
 
 </body>
 
--- arcoirsl_webstore/resources/views/partials/_header.blade.php
+++ arcoirsl_webstore/resources/views/partials/_header.blade.php
@@ -1,23 +1,20 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>{{ config('app.name') }}</title>
-
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/frontend.css?v=1') }}" media="all" />
-    <link rel="stylesheet" type="text/css" href="{{ asset('css/custom.css?v=4') }}" media="all" />
-    <link
-        href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,700italic,800,800italic,600italic,400italic,300italic'
-        rel='stylesheet' type='text/css'>
-        
-    <!-- Chrome, Firefox OS and Opera -->
-    <meta name="theme-color" content="#181818">
-    <!-- Windows Phone -->
-    <meta name="msapplication-navbutton-color" content="#181818">
-    <!-- iOS Safari -->
-    <meta name="apple-mobile-web-app-status-bar-style" content="black">
-
-    @stack('style')
-
-    <link rel="shortcut icon" href="{{ asset('frontend/images/fav-icon.png') }}">
-</head>
+<header class="header">
+    <div class="logo"></div>
+    <div class="tool-bar">
+        <div class="land">
+            <i class="fa-solid fa-language"></i>
+        </div>
+        <div class="searcher">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div class="user">
+            <i class="fa-regular fa-user"></i>
+        </div>
+        <div class="favourite">
+            <i class="fa-regular fa-heart"></i>
+        </div>
+        <div class="shopping">
+            <i class="fa-solid fa-bag-shopping"></i>
+        </div>
+    </div>
+</header>
