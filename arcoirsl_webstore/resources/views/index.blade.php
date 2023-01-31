76f2f5e22cf17840cd7fc7fd5fd5c3199948d709
Junjie_Li <jfjunjieli@gmail.com>
2023-01-31 20:05:38 +0100

    feat: header demo

--- arcoirsl_webstore/resources/views/index.blade.php
+++ arcoirsl_webstore/resources/views/index.blade.php
@@ -6,10 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>arcoirsl</title>
-
+    {{-- 
     <link href="../css/app.css" rel="stylesheet">
     <link href="../css/normalize.css" rel="stylesheet">
-    <link href="../css/tailwind.css" rel="stylesheet">
+    <link href="../css/tailwind.css" rel="stylesheet"> --}}
+
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/normalize.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/tailwind.css') }}" />
 
 </head>
 
