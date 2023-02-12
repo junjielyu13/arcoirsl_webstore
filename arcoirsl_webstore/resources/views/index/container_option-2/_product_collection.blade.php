00db297c99d60a916ba405de7e93ba47047a2b52
Junjie_Li <jfjunjieli@gmail.com>
2023-02-12 23:13:43 +0100

    feat: laravel container option2 flow

--- arcoirsl_webstore/resources/views/index/container_option-2/_product_collection.blade.php
+++ arcoirsl_webstore/resources/views/index/container_option-2/_product_collection.blade.php
@@ -1,1 +1,162 @@
-
+<div class="product-container">
+    <div class="product-container-col-sm">
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+    </div>
+    <div class="product-container-col-md">
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+    </div>
+    <div class="product-container-col-bg">
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+    </div>
+    <div class="product-container-col-md">
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+    </div>
+    <div class="product-container-col-sm">
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-sm">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-md">
+            <img src="../../public/img/product/IMG_2841.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2842.JPG" alt="" srcset="" />
+        </div>
+        <div class="product-bg">
+            <img src="../../public/img/product/IMG_2840.JPG" alt="" srcset="" />
+        </div>
+    </div>
+</div>
